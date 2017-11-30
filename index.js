84b60c879e7f25f0c44e8ce61bc991602838581f
JOOLEE <jooleechin@gmail.com>
2017-11-30 09:21:31 -0800

    demo complete

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let dogs = ['bowser', 'woof', 'dog3']
 let cats = ['meow', 'lexington', 'cat3']
 
 // add two new arrays of your choice
+let hamsters = ['mr.bonks', 'chirp', 'dwarf']
+let humans = ['joolee', 'chin', 'whatsuppup', 'ilovehamsters']
 
 app.get('/dogs', function(req, res) {
   res.json(dogs)
@@ -15,6 +17,13 @@ app.get('/cats', function(req, res) {
 })
 
 // add two new routes of your choice
+app.get('/hamsters', function(req, res) {
+    res.json(hamsters)
+})
+
+app.get('/humans', function(req, res) {
+    res.json(humans)
+})
 
 app.listen(3000, function() {
   console.log('listening on port 3000')
